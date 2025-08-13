be8d157f3afcfff4cd19b468607adb6ea9fd2e97
Matt Hunt <wmatthunt@gmail.com>
2025-08-13 10:38:30 -0400

    handle adde item

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
-import addItem from "./CartSlice"
+import {addItem,removeItem,updateQuantity} from "./CartSlice"
+import { useSelector, useDispatch } from "react-redux";
+
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
-
+    const dispatch = useDispatch();
     const plantsArray = [
         {
             category: "Air Purifying Plants",
