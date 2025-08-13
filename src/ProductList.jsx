c550dc0967907b8b92506704da233b76c0aef5b3
Matt Hunt <wmatthunt@gmail.com>
2025-08-13 10:30:18 -0400

    handleaddp

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import addItem from "./CartSlice"
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
@@ -233,7 +234,14 @@ function ProductList({ onHomeClick }) {
         fontSize: '30px',
         textDecoration: 'none',
     }
-
+    const handleAddToCart = (product) => 
+    {
+        dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
+        setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
+          ...prevState, // Spread the previous state to retain existing entries
+          [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
+        }));  
+    }
     const handleHomeClick = (e) => {
         e.preventDefault();
         onHomeClick();
